9c763ae89ceb2b3ddb6655a69a0080bed4d89ec8
TJ <tijojose27@yahoo.com>
2018-09-07 19:37:21 -0500

    added html files

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el:"#vue-app",
+  data:{
+    name: 'Tijo',
+    job:'Ninja',
+    website:'http://www.thenetninja.co.uk',
+    websiteTag: '<a href="http://www.google.com">GOOGLE</a>',
+  },
+  methods:{
+    greet:function (time) {
+      return 'Good '+time+" "+this.name;
+    }
+  }
+});
