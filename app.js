f3792fd562c4d3a6319192e9b93cd14e6cf2ea42
TJ <tijojose27@yahoo.com>
2018-09-08 23:11:35 -0500

    #11 Dynamic CSS

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 new Vue({
   el: "#vue-app",
   data: {
-    age: 21,
-    a:0,
-    b:0
+    available:false,
+    nearby:false
   },
   methods: {
     // addtoA:function () {
@@ -14,11 +13,11 @@ new Vue({
     // }
   },
   computed: {
-    addtoA:function () {
-      return this.a+this.age;
-    },
-    addtoB:function () {
-      return this.b+this.age;
+    compClasses: function () {
+      return {
+        available: this.available,
+        nearby:this.nearby
+      }
     }
   },
 });
